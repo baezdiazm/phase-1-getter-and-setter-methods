a64263a702c96ce77c22912322423084ad81827c
Manuel Baez Diaz <baezdiazm@yahoo.com>
2022-03-09 17:24:53 -0500

    commit tree 34e8501266e4623910ecc5620cf30dce8fa275df

--- index.js
+++ index.js
@@ -1,1 +1,31 @@
 // Add your Circle class here
+
+class Circle {
+    constructor (r) {
+        this.radius = r
+    }
+
+    get diameter() {
+        return this.radius * 2
+    }
+
+    set diameter(newdiameter) {
+        this.radius = newdiameter / 2
+    }
+
+    get circumference() {
+        return 2 * 3.14 * this.radius
+    }
+
+    set circumference(circ) {
+        this.radius = circ / 3.14 / 2
+    }
+
+    get area() {
+        return 3.14 * (this.radius * this.radius)
+    }
+
+    set area(area) {
+        this.radius = Math.sqrt(area / 3.14)
+    }
+}
